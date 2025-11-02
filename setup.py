f963a58109d6d2ca10afb8568cf31bec59b1c44a
Nis-hanth <nishanthshiva952@gmail.com>
2025-11-02 16:50:34 +0530

    requirements.added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_namespace_packages, setup
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Sunita Kumari",
+    author_email="nishanthshiva952.gmail.com",
+    packages=find_namespace_packages(),
+    install_requires=[]
+)
